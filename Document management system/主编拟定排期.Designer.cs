4bc3fb2d9a9a36b0c6ff9edd3089ffb0eab49d84
chen <2789385005@qq.com>
2024-07-20 01:22:24 +0800

    新增主编拟定稿件排期和教师决定排期界面

--- Document management system/主编拟定排期.Designer.cs
+++ Document management system/主编拟定排期.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dataGridView1 = new DataGridView();
+            BtnRevise = new Button();
+            BtnOk = new Button();
+            Close = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(43, 93);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(787, 337);
+            dataGridView1.TabIndex = 0;
+            // 
+            // BtnRevise
+            // 
+            BtnRevise.Font = new Font("黑体", 13.8F);
+            BtnRevise.Location = new Point(227, 472);
+            BtnRevise.Name = "BtnRevise";
+            BtnRevise.Size = new Size(94, 37);
+            BtnRevise.TabIndex = 1;
+            BtnRevise.Text = "修改";
+            BtnRevise.UseVisualStyleBackColor = true;
+            // 
+            // BtnOk
+            // 
+            BtnOk.Font = new Font("黑体", 13.8F);
+            BtnOk.Location = new Point(545, 472);
+            BtnOk.Name = "BtnOk";
+            BtnOk.Size = new Size(94, 37);
+            BtnOk.TabIndex = 2;
+            BtnOk.Text = "确定";
+            BtnOk.UseVisualStyleBackColor = true;
+            // 
+            // Close
+            // 
+            Close.BackgroundImage = Properties.Resources.关闭按钮;
+            Close.BackgroundImageLayout = ImageLayout.Stretch;
+            Close.Location = new Point(811, 12);
+            Close.Name = "Close";
+            Close.Size = new Size(58, 47);
+            Close.TabIndex = 3;
+            Close.UseVisualStyleBackColor = true;
             // 
             // 主编拟定排期
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(881, 552);
+            Controls.Add(Close);
+            Controls.Add(BtnOk);
+            Controls.Add(BtnRevise);
+            Controls.Add(dataGridView1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "主编拟定排期";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "主编拟定排期";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Button BtnRevise;
+        private Button BtnOk;
+        private Button Close;
     }
 }
--- Document management system/教师决定排期.Designer.cs
+++ Document management system/教师决定排期.Designer.cs
@@ -28,21 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Close = new Button();
+            BtnRevise = new Button();
+            BtnOk = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // Close
+            // 
+            Close.BackgroundImage = Properties.Resources.关闭按钮;
+            Close.BackgroundImageLayout = ImageLayout.Stretch;
+            Close.Location = new Point(773, 12);
+            Close.Name = "Close";
+            Close.Size = new Size(52, 51);
+            Close.TabIndex = 0;
+            Close.UseVisualStyleBackColor = true;
+            // 
+            // BtnRevise
+            // 
+            BtnRevise.Font = new Font("黑体", 13.8F);
+            BtnRevise.Location = new Point(214, 414);
+            BtnRevise.Name = "BtnRevise";
+            BtnRevise.Size = new Size(94, 40);
+            BtnRevise.TabIndex = 1;
+            BtnRevise.Text = "修改";
+            BtnRevise.UseVisualStyleBackColor = true;
+            // 
+            // BtnOk
+            // 
+            BtnOk.Font = new Font("黑体", 13.8F);
+            BtnOk.Location = new Point(545, 414);
+            BtnOk.Name = "BtnOk";
+            BtnOk.Size = new Size(94, 40);
+            BtnOk.TabIndex = 2;
+            BtnOk.Text = "确定";
+            BtnOk.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(50, 69);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(734, 317);
+            dataGridView1.TabIndex = 3;
             // 
             // 教师决定排期
             // 
-            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(416, 351);
+            ClientSize = new Size(837, 479);
+            Controls.Add(dataGridView1);
+            Controls.Add(BtnOk);
+            Controls.Add(BtnRevise);
+            Controls.Add(Close);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(4);
             Name = "教师决定排期";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "教师决定排期";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button Close;
+        private Button BtnRevise;
+        private Button BtnOk;
+        private DataGridView dataGridView1;
     }
 }
